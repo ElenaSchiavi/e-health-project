df22db05256eceb3bed5cd9219e32137d400326c
LeonardoPellecchia <leonardoaltay.pellecchia@mail.polimi.it>
2024-12-03 16:24:05 +0100

    inizio discorsi

---
+++ Assets/TESTING/SceneScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewStoryScene", menuName = "Data/New Story Scene")]
+[System.Serializable]
+public class StoryScene : ScriptableObject
+{
+    public List<Sentence> sentences;
+    public Sprite background;
+    public StoryScene nextScene;
+
+    [System.Serializable]
+    public struct Sentence
+    {
+        public string text;
+        public Speaker speaker;
+    }
+}
