e65b0e90409420a6d274a95cd49a2fdaca662ab1
Elena Schiavi <elena1.schiavi@mail.polimi.it>
2024-12-12 22:44:41 +0100

    CORRETTO

--- Assets/SCENES/DAY 1/Avid Smoker/1. AS - Bedroom/Scripts/CharacterMover.cs
+++ Assets/SCENES/DAY 1/Avid Smoker/1. AS - Bedroom/Scripts/CharacterMover.cs
@@ -6,7 +6,6 @@ public class CharacterMover : MonoBehaviour
     public float speed = 700f; 
     public Vector2 offScreenLeft = new Vector2(-733f, -125f);
     public Vector2 scenePosition = new Vector2(-450f, -125f);
-    public Vector2 offScreenRight = new Vector2(733f, -125f);
 
     private bool isMovingToScene = true;
     private bool isExitingScene = false;
@@ -20,30 +19,26 @@ public class CharacterMover : MonoBehaviour
     {
         if (isMovingToScene)
         {
-            uiObject.anchoredPosition = Vector2.MoveTowards(
-                uiObject.anchoredPosition,
-                scenePosition,
-                speed * Time.deltaTime
-            );
-
-            if (uiObject.anchoredPosition == scenePosition)
-            {
-                isMovingToScene = false;
-            }
+            MoveToPosition(scenePosition, ref isMovingToScene);
         }
 
         if (isExitingScene)
         {
-            uiObject.anchoredPosition = Vector2.MoveTowards(
-                uiObject.anchoredPosition,
-                offScreenRight,
-                speed * Time.deltaTime
-            );
+            MoveToPosition(offScreenLeft, ref isExitingScene);
+        }
+    }
 
-            if (uiObject.anchoredPosition == offScreenRight)
-            {
-                isExitingScene = false;
-            }
+    private void MoveToPosition(Vector2 targetPosition, ref bool toggleFlag)
+    {
+        uiObject.anchoredPosition = Vector2.MoveTowards(
+            uiObject.anchoredPosition,
+            targetPosition,
+            speed * Time.deltaTime
+        );
+
+        if (uiObject.anchoredPosition == targetPosition)
+        {
+            toggleFlag = false;
         }
     }
 
