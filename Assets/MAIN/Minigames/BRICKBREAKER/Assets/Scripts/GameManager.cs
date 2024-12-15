4accc14dd5b23aef76a4601a368b55eda0b8c2d4
MatteoFranchi01 <matteo.franchi31101@gmail.com>
2024-12-15 17:52:59 +0100

    Update Minigames w threshoolds

--- Assets/MAIN/Minigames/BRICKBREAKER/Assets/Scripts/GameManager.cs
+++ Assets/MAIN/Minigames/BRICKBREAKER/Assets/Scripts/GameManager.cs
@@ -10,11 +10,11 @@ public class GameManager : MonoBehaviour
     public GameObject m_GameMenuPanel;
     public GameObject m_GameOverPanel;
     public GameObject m_Scores;
-    public GameObject gameOverText; // Riferimento alla scritta Game Over
-    public GameObject winText;      // Riferimento alla scritta Win
+    public GameObject gameOverText; 
+    public GameObject winText;      
     public Text m_GameOverFinalScore;
     private static int neededscore = 0;
-    private static int setmood = 0; //LEO MODIFICA SET MOOD CON IL VALORE SOGLIA DEL MOOD,M >= SETM0OOD VUOL DIRE CHE è FELICE 
+    private static int setmood = 0; 
     private static int setsigarette = 1;
 
     public enum GameState { MainMenu, Playable, GameOver, }
--- Assets/MAIN/Minigames/PLANE/Scripts/PlaneGameManager.cs
+++ Assets/MAIN/Minigames/PLANE/Scripts/PlaneGameManager.cs
@@ -13,7 +13,7 @@ public class PlaneGameManager : MonoBehaviour
     private static int attempts = 0;
     private const int maxAttempts = 3;
     public float winTime = 0f;
-    private static int setmood = 0; //LEO MODIFICA SET MOOD CON IL VALORE SOGLIA DEL MOOD,M >= SETM0OOD VUOL DIRE CHE è FELICE 
+    private static int setmood = 0; 
     private static int setsigarette = 1;
 
     private bool isGameOver = false;
